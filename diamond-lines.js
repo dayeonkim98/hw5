86a96afba766f24a85d1a0f8f8c4bf9877ad368d
dayeonkim98 <43260719+dayeonkim98@users.noreply.github.com>
2018-10-11 22:18:29 -0700

    Create diamond-lines.js

---
+++ diamond-lines.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 10; i <= 390; i = i + 10) {
+    line(200, i, 200+(), i);
+  }
+}
+I REALLY CANNOT RIGURE OUT DIAMOND....
